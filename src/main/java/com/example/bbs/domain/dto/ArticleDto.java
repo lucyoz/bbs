d9d883ae83916a5f6e14f740568ba02a21e29a45
lucyoz <lucyoz.kwon@gmail.com>
2022-11-10 13:34:52 +0900

    오류 해결 (DB에 id를 제외한 컬럼에 null값만 저장됨.)  - No key, method or field with name 'title' on line 14

--- src/main/java/com/example/bbs/domain/dto/ArticleDto.java
+++ src/main/java/com/example/bbs/domain/dto/ArticleDto.java
@@ -4,8 +4,10 @@ import com.example.bbs.domain.Article;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ArticleDto {
